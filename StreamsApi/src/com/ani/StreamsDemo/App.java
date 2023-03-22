875b50b4ffc0a8b32283feba7f0db47a721f9b90
Rithvik31 <rithvikchinnu@gmail.com>
2023-03-22 12:11:58 +0530

    coding questions

---
+++ StreamsApi/src/com/ani/StreamsDemo/App.java
@@ -0,0 +1,13 @@
+package com.ani.StreamsDemo;
+
+import com.ani.stream.StreamDemos;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("🔥 Streams API 🔥");
+
+        StreamDemos sd = new StreamDemos();
+        sd.demo11();
+
+    }
+}
