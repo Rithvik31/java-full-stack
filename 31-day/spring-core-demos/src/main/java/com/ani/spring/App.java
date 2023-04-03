6c1418b05d7ed7632a2002d7a571be9a2053ce68
Rithvik31 <rithvikchinnu@gmail.com>
2023-04-03 12:12:56 +0530

    Spring DI : java and xml

---
+++ 31-day/spring-core-demos/src/main/java/com/ani/spring/App.java
@@ -0,0 +1,46 @@
+package com.ani.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ani.spring.config.AppConfig;
+import com.ani.spring.util.FileChecker;
+import com.ani.spring.util.FileUtil;
+
+public final class App {
+
+    public static void normal() {
+        System.out.println("🔥 Providing Dependencies Normally 🔥");
+
+        FileUtil util = new FileUtil();
+
+        FileChecker checker = new FileChecker(util);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringJavaConfig() {
+        System.out.println("🔥 Providing Dependencies With Java Code 🔥");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] names = ctx.getBeanDefinitionNames();
+        for (String nm : names) {
+            System.out.println(" 👉 " + nm);
+        }
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringXmlConfig() {
+        System.out.println("🔥 Providing Dependencies With Xml Config 🔥");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void main(String[] args) {
+        withSpringXmlConfig();
+    }
+}
