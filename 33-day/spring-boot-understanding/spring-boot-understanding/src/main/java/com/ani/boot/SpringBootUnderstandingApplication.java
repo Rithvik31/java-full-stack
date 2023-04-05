caa0c59e69068a75aaace0ecb45f69ab70862826
Rithvik31 <rithvikchinnu@gmail.com>
2023-04-05 13:18:19 +0530

    spring boot : add starter

--- 33-day/spring-boot-understanding/spring-boot-understanding/src/main/java/com/ani/boot/SpringBootUnderstandingApplication.java
+++ 33-day/spring-boot-understanding/spring-boot-understanding/src/main/java/com/ani/boot/SpringBootUnderstandingApplication.java
@@ -1,5 +1,6 @@
 package com.ani.boot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +19,7 @@ public class SpringBootUnderstandingApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SpringBootUnderstandingApplication.class, args);
 
-		CarService service =  ctx.getBean(CarService.class);
+		CarService service = ctx.getBean(CarService.class);
 		service.createNewCar(10L, "abc", "lmn");
 	}
 }
