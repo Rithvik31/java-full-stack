ddaa8013750fc0fc8709b837f30268d3256acea1
Rithvik31 <rithvikchinnu@gmail.com>
2023-04-14 11:57:43 +0530

    spring data : crud operations

--- 40-day/spring-data-demo/src/main/java/com/ani/data/SpringDataDemoApplication.java
+++ 40-day/spring-data-demo/src/main/java/com/ani/data/SpringDataDemoApplication.java
@@ -2,12 +2,18 @@ package com.ani.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.ani.data.demo.CrudDemo;
 
 @SpringBootApplication
 public class SpringDataDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDataDemoApplication.class, args);
+
+		CrudDemo demo = ctx.getBean(CrudDemo.class);
+		demo.demo1();
 	}
 
 }
